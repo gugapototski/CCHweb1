33325d8cefbd34b1684556ec78bb50b699784292
gugapototski <gugapototski@live.com>
2023-06-08 17:09:18 -0300

    srcipt.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function enviarFormulario(event) {
   const email = emailInput.value;
 
   // Lógica para enviar o formulário ou processar os dados
+  const dadosFormulario = {
+    email: email,
+    //Outros campos do formulário
+  };
+
+  // Simulação do envio do formulário
+  console.log("Dados do formulário:", dadosFormulario);
 
   // Exemplo de exibição de mensagem de sucesso
   alert(`Formulário enviado com sucesso para o e-mail: ${email}`);
